52d7bb4f9de93c74de80ca13bf90073d63879d1f
GitPS <sime.phil@uwlax.edu>
2014-11-07 22:08:17 -0600

    Initial commit from local git

---
+++ CycleTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CycleTracker
+//
+//  Created by Phillip Sime on 11/7/14.
+//  Copyright (c) 2014 Phil Sime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CycleTracker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CycleTracker
+//
+//  Created by Phillip Sime on 11/7/14.
+//  Copyright (c) 2014 Phil Sime. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

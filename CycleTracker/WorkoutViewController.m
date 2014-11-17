f6cec7ea1bb841128389900f3283e1f238d95f12
GitPS <sime.phil@uwlax.edu>
2014-11-17 10:54:07 -0600

    Changes to timer. Addition of cadence output label. Fix pause/unpause button

--- CycleTracker/WorkoutViewController.m
+++ CycleTracker/WorkoutViewController.m
@@ -9,12 +9,14 @@
 #import "WorkoutViewController.h"
 
 @interface WorkoutViewController ()
+
 @property (strong, nonatomic) IBOutlet UIButton *startButton;
 @property (strong, nonatomic) IBOutlet UIButton *pauseButton;
 @property (strong, nonatomic) IBOutlet UIButton *stopButton;
 @property (strong, nonatomic) IBOutlet UILabel *timerLabel;
+@property (strong, nonatomic) IBOutlet UILabel *cadenceLabel;
 @property (strong, nonatomic) NSTimer * timer;
-@property int timerValue;
+@property double timerValue;
 
 @end
 
@@ -22,15 +24,23 @@
 
 # pragma mark Timer
 
+//- (void) incrementTimer {
+//    NSLog(@"incrementTimer");
+//    _timerLabel.text = [NSString stringWithFormat:@"%d", ++_timerValue];
+//}
+
 - (void) incrementTimer {
-    NSLog(@"incrementTimer");
-    _timerLabel.text = [NSString stringWithFormat:@"%d", ++_timerValue];
+    _timerValue += 0.1;
+    double seconds = fmod(_timerValue, 60.0);
+    double minutes = fmod(trunc(_timerValue / 60.0), 60.0);
+    double hours = trunc(_timerValue / 3600.0);
+    self.timerLabel.text = [NSString stringWithFormat:@"%02.0f:%02.0f:%04.1f", hours, minutes, seconds];
 }
 
 - (void) resetTimer {
     NSLog(@"resetTimer");
     _timerValue = 0;
-    _timerLabel.text = [NSString stringWithFormat:@"%d", _timerValue];
+    _timerLabel.text = @"00:00:00:0";
 }
 
 # pragma mark Buttons
@@ -41,21 +51,21 @@
     // If the timer is already running don't do anything
     if(_timer) return;
     
-    _timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector: @selector(incrementTimer) userInfo:NULL repeats:YES];
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector: @selector(incrementTimer) userInfo:NULL repeats:YES];
 }
 
 - (IBAction)pauseWorkout:(id)sender {
     NSLog(@"pauseWorkout");
     NSString *title = [_pauseButton titleForState:UIControlStateNormal];
     
-    // If the timer is not running and not paused don't do anything
+    // If the timer is not yet created don't do anything
     if(!_timer) return;
     
     if([title isEqualToString:@"Pause"]) {
         [_timer invalidate];
         [_pauseButton setTitle:@"Resume" forState:UIControlStateNormal];
     } else {
-        _timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector: @selector(incrementTimer) userInfo:NULL repeats:YES];
+        _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector: @selector(incrementTimer) userInfo:NULL repeats:YES];
         [_pauseButton setTitle:@"Pause" forState:UIControlStateNormal];
     }
 }
@@ -65,8 +75,27 @@
     if(_timer) {
         [_timer invalidate];
         _timer = NULL;
-        _timerLabel.text = @"0";
+        _timerLabel.text = @"00:00:00:0";
         _timerValue = 0;
+        // Ensure the pause button is reset
+        [_pauseButton setTitle:@"Pause" forState:UIControlStateNormal];
+        // Ensure the cadence label is reset
+        [self updateCadenceLabel:0];
+    }
+}
+
+# pragma mark Cadence Logic
+
+- (void)updateCadenceLabel:(int)cadence {
+    int goalCadence = 80;
+    if(cadence < goalCadence) {
+        UIColor *color = [UIColor redColor];
+        self.cadenceLabel.textColor = color;
+        self.cadenceLabel.text = [NSString stringWithFormat:@"%d RPM", cadence];
+    } else {
+        UIColor *color = [UIColor greenColor];
+        self.cadenceLabel.textColor = color;
+        self.cadenceLabel.text = [NSString stringWithFormat:@"%d RPM", cadence];
     }
 }
 
@@ -81,13 +110,13 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
